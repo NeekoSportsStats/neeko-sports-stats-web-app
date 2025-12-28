d8eb623a2e940dd50a48130c5ff111b9247bda49
Matthew Nixon <matthew@neekostats.com.au>
2025-12-28 23:13:06 +1100

    .319

--- eslint.config.js
+++ eslint.config.js
@@ -24,3 +24,19 @@ export default tseslint.config(
     },
   },
 );
+import unusedImports from "eslint-plugin-unused-imports";
+
+export default [
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+    },
+    rules: {
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" }
+      ],
+    },
+  },
+];
