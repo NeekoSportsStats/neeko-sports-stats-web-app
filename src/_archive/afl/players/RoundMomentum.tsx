a747e0e8fab7ec17846f666d3c3e8cda22d44cdc
NeekoSportsStats <matthew@neekostats.com.au>
2026-01-18 03:33:21 +1000

    Delete RoundMomentum.tsx

--- src/_archive/afl/players/RoundMomentum.tsx
+++
@@ -1,6 +0,0 @@
-import RoundSummary from "@/components/afl/players/Section-6-overview/RoundSummary";
-import { AFL_STAT_CONFIG } from "@/lib/stats/afl/statConfig";
-
-export default function RoundMomentum() {
-  return <RoundSummary statConfig={AFL_STAT_CONFIG} />;
-}
